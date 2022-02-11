c09cc50b118134a2f4e46fc9f8cb4b5d0773846b
Your usernamemichaelcurry1 <michael.curry@bryanuniversity.edu>
2022-02-10 20:59:04 -0500

    2ndProject

--- week3/user.js
+++ week3/user.js
@@ -5,49 +5,30 @@ module.exports = class User{
         this.num1 = num1,
         this.num2 = num2
     }
-    addBy()
-{   console.log("add", this.num1, this.num2)
+
+    addBy(){   console.log("add", this.num1, this.num2)
     return this.num1 + this.num2
-        /* num1 = document.getElementById("firstNumber").value;
-        num2 = document.getElementById("secondNumber").value;
-        document.getElementById("result").innerHTML = num1 + num2; */
-        
-}
+    }
 
-    subtractBy() 
-
-{ 
+    subtractBy(){ 
     return this.num1 - this.num2
-        /* num1 = document.getElementById("firstNumber").value;
-        num2 = document.getElementById("secondNumber").value;
-        document.getElementById("result").innerHTML = num1 - num2; */
-
-}
-     multiplyBy()
-{
+    }
+    
+        multiplyBy(){
     return this.num1 * this.num2
-       /*  num1 = document.getElementById("firstNumber").value;
-        num2 = document.getElementById("secondNumber").value;
-        document.getElementById("result").innerHTML = num1 * num2; */
-}
+    }
 
-     divideBy() 
-{ 
+     divideBy(){ 
     return this.num1 / this.num2
-       /*  num1 = document.getElementById("firstNumber").value;
-        num2 = document.getElementById("secondNumber").value;
-        document.getElementById("result").innerHTML = num1 / num2; */
-}
-nameAge = () => { return `Hello ${this.userName}. You are ${this.userAge} years young!`
+    }
 
-}
-/* nameAge(){
-    return this.userName
-} */
+nameAge = () => { return `Hello ${this.userName}. You are ${this.userAge} years young!`
+    }
+
 dataType = (arg) => { return `${arg} is a ${typeof(arg)}!`}
 
 turnNumberToString = (num) => {
     let userAgeToString = String(num) 
     return userAgeToString
-}
+    }
 }
